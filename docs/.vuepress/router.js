05279751f7b48c1e773d51ac59841c500d93bb60
王修园 <xiuyuan.wang@mayihr.com>
2020-09-25 09:29:34 +0800

    fix: 大大

---
+++ docs/.vuepress/router.js
@@ -0,0 +1,19 @@
+module.exports = {
+  '/days/': [],
+  '/vueUIDoc/': [
+    ['/components/validCode', '短信验证码'],
+    {
+      title: '组件',
+      collapsable: true,
+      children: [{
+        title: '短信验证码',
+        path: 'validCode',
+        collapsable: true,
+        children: [
+          ['/components/validCode', '短信验证码']
+        ]
+      }]
+    },  
+  ],
+  '/knows/': [],
+};
